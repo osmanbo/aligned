d0f61921973214175e9e7859f77c58871c45052e
Osman Bozkurt <55315699+osmanbo@users.noreply.github.com>
2024-06-18 14:25:10 +0300

    Add files via upload

---
+++ metadata.js
@@ -0,0 +1,7 @@
+{
+  "name": "0xosman",
+  "website": "https://github.com/osmanbo",
+  "description": "Don't Trust, Verify",
+  "logo": "https://pbs.twimg.com/profile_images/1722976497764335616/3yWgLp0X_400x400.jpg",
+  "twitter": "https://twitter.com/0xosman_"
+}
